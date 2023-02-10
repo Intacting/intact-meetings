4f324fd948a5b07f8fd7e506eaee28195a76e908
Apple <apple@192.168.1.215>
2023-02-10 15:01:46 +0530

    build generate

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/intact-meetings">
     <React.StrictMode>
       <App />
     </React.StrictMode>
