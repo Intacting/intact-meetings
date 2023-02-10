cebc326aa8e2598bbfb1cc4594ef3cc553d3b6a8
Apple <apple@192.168.1.215>
2023-02-10 16:22:32 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -15,19 +15,26 @@ import EditEventType from "./Pages/UserDashBoard/EditEventType";
 function App() {
   return (
     <>
-    
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/intact-meetings/app/login/password" element={<Password/>}></Route>
-        <Route path="/user" element={<Dashboard/>}></Route>
-        <Route path="/user/create_event_type" element={<CreateEventType/>}></Route>
-        <Route path="/user/create_event_type/one-to-one" element={<AddOneonOneEvent/>}></Route>
-        <Route path="/user/create_event_type/group_event" element={<AddGroupEvent/>}></Route>
-        <Route path="/user/add_workflow" element={<AddNewWorkflow/>}></Route>
-        <Route path="/user/edit_event" element={<EditEventType/>}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/app/login/password" element={<Password />}></Route>
+        <Route path="/user" element={<Dashboard />}></Route>
+        <Route
+          path="/intact-meetings/user/create_event_type"
+          element={<CreateEventType />}
+        ></Route>
+        <Route
+          path="/user/create_event_type/one-to-one"
+          element={<AddOneonOneEvent />}
+        ></Route>
+        <Route
+          path="/user/create_event_type/group_event"
+          element={<AddGroupEvent />}
+        ></Route>
+        <Route path="/user/add_workflow" element={<AddNewWorkflow />}></Route>
+        <Route path="/user/edit_event" element={<EditEventType />}></Route>
       </Routes>
-    
     </>
   );
 }
